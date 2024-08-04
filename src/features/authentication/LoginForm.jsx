ec824efa6f2b00c453b27ec2a58b2cab10a5a93f
MI-Progammer <ilhamdivine7@gmail.com>
2024-08-04 09:45:09 +0700

    Update, LoginForm component

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -9,8 +9,8 @@ import SpinnerMini from "../../ui/SpinnerMini";
 import { useLogin } from "./useLogin";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("user@example.com");
+  const [password, setPassword] = useState("12345678");
   const { login, isLogging } = useLogin();
 
   function handleSubmit(e) {
